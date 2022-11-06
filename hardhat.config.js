c024b518945d120b7c6e5674dd1d737567217638
marcialarturo <87619763+marcialarturo@users.noreply.github.com>
2022-11-05 23:01:02 -0400

    add config

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require('@nomiclabs/hardhat-waffle')
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.10',
+  networks: {
+    hardhat: {
+      chainId: 1337, //matic
+      // chainId: 0xf45db2a, //skale
+    },
+    matic: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: ['YOUR PRIVATE KEY FROM METAMASK'],
+    },
+    skale: {
+      url: 'https://eth-online.skalenodes.com/v1/hackathon-content-live-vega',
+      accounts: [''],
+    },
+    optimistic: {
+      url:
+        'https://opt-kovan.g.alchemy.com/v2/cfv68qUqLuZipE8sXJYuG-KQ1eKiuPsl',
+      accounts: ['YOUR PRIVATE KEY FROM METAMASK'],
+    },
+    rinkeby: {
+      url:
+        'https://eth-rinkeby.gateway.pokt.network/v1/lb/62b8802d123e6f0039866020',
+      accounts: [''],
+    },
+  },
+  paths: {
+    artifacts: './src/artifacts',
+    cache: './src/cache',
+  },
+  NODE_OPTIONS: '--openssl-legacy-provider',
+}
